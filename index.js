d633cc5b88991058fc7092a58175d72dde827185
Amar Khan <amarkotasky@gmail.com>
2024-05-21 10:16:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const { run } = require("@probot/adapter-github-actions");
 const app = require("./app");
 
+
+fs.writeFile('/Users/joe/test.txt', content, err => {
+  if (err) {
+    console.error(err);
+  } else {
+    // file written successfully
+  }
+});
+
 run(app);
